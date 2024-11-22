9115709110ac240afa2203dea68904a35b922bdb
Patrick Latimer <patrick.latimer@alleninstitute.org>
2024-11-22 11:35:44 -0800

    Add --device-yaml arg with default

--- software/python/src/harp_env_sensor/main.py
+++ software/python/src/harp_env_sensor/main.py
@@ -13,6 +13,60 @@ from pyharp.device import DeviceMode, Device
 
 from harp_env_sensor import EnvSensorDevice, __version__
 
+default_yaml = """
+%YAML 1.1
+---
+# yaml-language-server: $schema=https://harp-tech.org/draft-02/schema/device.json
+device: EnvironmentSensor
+whoAmI: 1405
+firmwareVersion: "0.1"
+hardwareTargets: "0.1"
+registers:
+  Pressure:
+    access: Read
+    address: 32
+    type: U32
+    description: Pressure, in Pa
+  Temperature:
+    access: Read
+    address: 33
+    type: Float
+    description: Temperature in degrees C
+  Humidity:
+    access: Read
+    address: 34
+    type: Float
+    description: Humidity, in %RH
+  SensorData:
+    access: Event
+    address: 35
+    type: Float
+    description: A periodic event will be emitted with aggregated data from all sensors.
+    length: 3
+    payloadSpec:
+      Pressure:
+        offset: 0
+        description: Pressure, in Pa
+      Temperature:
+        offset: 1
+        description: Temperature in degrees C
+      Humidity:
+        offset: 2
+        description: Humidity, in %RH
+  EnableEvents:
+    access: Write
+    address: 36
+    type: U8
+    maskType: Events
+    description: Enables (~2Hz) or disables the SensorData events
+bitMasks:
+  Events:
+    bits:
+      Disable: 0x0
+      SensorData: 0x1
+    description: Available events on the device
+"""
+
 
 def read_once(device: EnvSensorDevice) -> tuple[float, float, float]:
     pressure = device.Pressure[0]
@@ -58,6 +112,7 @@ def cli() -> argparse.Namespace:
     )
 
     parser.add_argument("-v", "--version", action="version", version=__version__)
+    parser.add_argument("--device-yaml", type=os.path.abspath, default=None)
     parser.add_argument("--comport", type=str, help="Serial port where the device is connected", default=None)
     parser.add_argument("--file", type=os.path.abspath)
     parser.add_argument("--mode", choices=["single", "continuous", "events"], default="single")
@@ -76,9 +131,14 @@ if __name__ == "__main__":
         print("\n".join(ports) + "\n")
         args.comport = port = input("Which COM port is your device on?")
 
-    device_yaml_path = Path(__file__).parents[4] / "device.yml"
-    with open(device_yaml_path, "r") as file:
-        device_yaml = yaml.safe_load(file)
+    if args.device_yaml is None:
+        device_yaml = yaml.safe_load(default_yaml)
+    else:
+        # device_yaml_path = Path(__file__).parents[4] / "device.yml"
+        with open(args.device_yaml, "r") as file:
+            device_yaml = yaml.safe_load(file)
+
+    print(device_yaml["firmwareVersion"])
 
     config_dict = {
         "dump_file_path": None,
